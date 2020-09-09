d8b96471460813d8f53ed2d03521a63069bf1ed2
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-09 22:23:32 +0300

    add Example

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter2/Example.java
@@ -0,0 +1,10 @@
+package main.java.com.Kuro4KinG.javacore.chapter2;
+/*
+    Это простая программа на Java.
+ */
+public class Example {
+    //Вызов метода main ()
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+}
