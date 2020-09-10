d8ae851f46ac9c43f1b0df0d8a1e24d0d0fd88c9
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-10 08:46:54 +0300

    add ForTest

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter2/ForTest.java
@@ -0,0 +1,14 @@
+package main.java.com.Kuro4KinG.javacore.chapter2;
+/*
+Продемонстриовать применение цикла for
+Присвоить исходному файлу имя "ForTest.java"
+ */
+
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for(x = 0; x < 10; x++)
+            System.out.println("Значение x: " + x);
+    }
+}
