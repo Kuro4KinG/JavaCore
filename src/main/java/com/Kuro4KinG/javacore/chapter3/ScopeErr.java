682410ce82b8a1138751893d9fb32f6df8602834
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-11 08:05:05 +0300

    add ScopeErr

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter3/ScopeErr.java
@@ -0,0 +1,11 @@
+package main.java.com.Kuro4KinG.javacore.chapter3;
+// Скомпилировать эту программу нельзя
+class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {                   // создаётся новая область действия
+            int bar = 2;    // Ошибка во вермя компиляции -
+                            // переменная bar уже определена!
+        }
+    }
+}
