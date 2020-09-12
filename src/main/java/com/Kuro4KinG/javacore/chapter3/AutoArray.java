69b095598494517bb91d7e04dc149da3af3d4f10
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-12 13:42:51 +0300

    add AutoArray

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter3/AutoArray.java
@@ -0,0 +1,9 @@
+package main.java.com.Kuro4KinG.javacore.chapter3;
+// Усовершенствованная версия предыдущей программы
+class AutoArray {
+    public static void main(String[] args) {
+
+        int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("В апреле " + month_days[3] + " дней.");
+    }
+}
