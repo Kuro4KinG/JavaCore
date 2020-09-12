60430d74ff8e09d47044ec62ac94485bae89fc8b
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-12 11:49:28 +0300

    add Conversion

--- src/main/java/com/Kuro4KinG/javacore/chapter3/ScopeErr.java
+++ src/main/java/com/Kuro4KinG/javacore/chapter3/ScopeErr.java
@@ -4,7 +4,7 @@ class ScopeErr {
     public static void main(String[] args) {
         int bar = 1;
         {                   // создаётся новая область действия
-            int bar = 2;    // Ошибка во вермя компиляции -
+        //    int bar = 2;    // Ошибка во вермя компиляции -
                             // переменная bar уже определена!
         }
     }
