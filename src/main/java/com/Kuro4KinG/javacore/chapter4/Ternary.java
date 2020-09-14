e187e09353695011f5a4c48a0a80bb0517d26868
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-14 20:13:54 +0300

    add Ternary

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter4/Ternary.java
@@ -0,0 +1,19 @@
+package main.java.com.Kuro4KinG.javacore.chapter4;
+
+// Продемонстирровать применение тернарной операции
+class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i; // получить абсолютное значение переменной i
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i; // получить абсолютное значение переменной i
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+    }
+}
