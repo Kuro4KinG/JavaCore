cc6fa13560adb99a81931f735c7949186f527fcd
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-14 21:53:42 +0300

    add While

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter5/While.java
@@ -0,0 +1,12 @@
+package main.java.com.Kuro4KinG.javacore.chapter5;
+
+// Продемонстрировать примениние оператора цикла while
+class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("такт " + n);
+            n--;
+        }
+    }
+}
