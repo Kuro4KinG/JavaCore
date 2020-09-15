c47a9db1e95d7f3f17199947144c77b46f088bfd
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-15 07:36:47 +0300

    modify ForTick

--- src/main/java/com/Kuro4KinG/javacore/chapter5/ForTick.java
+++ src/main/java/com/Kuro4KinG/javacore/chapter5/ForTick.java
@@ -1,11 +1,10 @@
 package main.java.com.Kuro4KinG.javacore.chapter5;
 
-// Продемонстрировать применение оператора цикла for
+// Объявить переменную управления циклом в самом цикле for
 class ForTick {
     public static void main(String[] args) {
-        int n;
-
-        for (n = 10; n > 0; n--)
+        // здесь переменная n объявляется в самом цикле for
+        for (int n = 10; n > 0; n--)
             System.out.println("такт " + n);
     }
 }
