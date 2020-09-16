5e3c24b087520152e31672b1744acb33bc0cb439
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-16 09:25:51 +0300

    add BoxDemo

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter6/BoxDemo.java
@@ -0,0 +1,27 @@
+package main.java.com.Kuro4KinG.javacore.chapter6;
+
+/*  Программа, использующая класс Box
+
+    Присвоить исходному файлу имя BoxDemo.java
+ */
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+// В этом классе объявляется объект типа Box
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box mybox = new Box();
+        double vol;
+        // присвоить значение переменным экземпляра mybox
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        // рассчитать объем параллелепипеда
+        vol = mybox.width * mybox.height * mybox.depth;
+        System.out.println("Объём равен " + vol);
+    }
+}
