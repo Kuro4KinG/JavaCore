146c0d6b8a3bae93a57c0c9137f51b5e156d69ec
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-16 07:13:30 +0300

    add BreakLoop2

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter5/BreakLoop2.java
@@ -0,0 +1,16 @@
+package main.java.com.Kuro4KinG.javacore.chapter5;
+
+// Применение оператора break для выхода из цикла while
+class BreakLoop2 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 100) {
+            if (i == 10) break; // выход из цикла, если значение
+                                // переменной i равно 10
+            System.out.println("i: " + i);
+            i++;
+        }
+        System.out.println("Цикл завершен.");
+    }
+}
