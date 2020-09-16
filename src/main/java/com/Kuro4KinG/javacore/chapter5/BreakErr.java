a359379673dc9702f77da1e2bd2594dca9b13c40
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-16 07:50:24 +0300

    add BreakErr

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter5/BreakErr.java
@@ -0,0 +1,16 @@
+package main.java.com.Kuro4KinG.javacore.chapter5;
+
+// Эта программа содержит ошибку
+class BreakErr {
+    public static void main(String[] args) {
+
+        one:
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Проход " + i + ": ");
+        }
+        for (int j = 0; j < 100; j++) {
+            // if (j == 10) break one; // ОШИБКА!
+            System.out.print(j + " ");
+        }
+    }
+}
