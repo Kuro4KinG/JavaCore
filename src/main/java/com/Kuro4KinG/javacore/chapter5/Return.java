ef9fbd221fc54d0fb358a89fb38d1c21e2bdbb43
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-16 08:46:20 +0300

    add Return

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter5/Return.java
@@ -0,0 +1,14 @@
+package main.java.com.Kuro4KinG.javacore.chapter5;
+
+// Продемонстрировать применение оператора return
+class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        System.out.println("До возврата.");
+
+        if (t) return; // возврат в вызывающий код
+
+        System.out.println("Этот оператор выполняться не будет.");
+    }
+}
