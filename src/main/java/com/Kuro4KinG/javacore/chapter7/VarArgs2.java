99f6ea9c92e2fb934fa629932b562e12b7ebc4ec
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-21 21:39:16 +0300

    add VarArgs2

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter7/VarArgs2.java
@@ -0,0 +1,22 @@
+package main.java.com.Kuro4KinG.javacore.chapter7;
+
+// Использовать аргументы переменной длины вместе
+// со стандартными аргументами
+class VarArgs2 {
+
+    // В данном примере msg - обычный параметр,
+    // а v - параметр переменной длины
+    static void vaTest(String msg, int... v) {
+        System.out.print(msg + v.length + " Содержимое: ");
+        for (int x : v)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest("Один параметр переменной длины: ", 10);
+        vaTest("Три параметра переменной длины: ", 1, 2, 3);
+        vaTest("Без параметров переменной длины: ");
+    }
+}
