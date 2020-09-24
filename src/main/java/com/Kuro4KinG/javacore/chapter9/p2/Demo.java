69e2e7f48c43892c02bdcb430aa21b833dd0ba6a
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-24 19:15:23 +0300

    add Demo

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter9/p2/Demo.java
@@ -0,0 +1,9 @@
+package main.java.com.Kuro4KinG.javacore.chapter9.p2;
+
+// получить экземпляры различных классов из пакета p2
+public class Demo {
+    public static void main(String[] args) {
+        Protection2 ob1 = new Protection2();
+        OtherPackage ob2 = new OtherPackage();
+    }
+}
