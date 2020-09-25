c79c3153c5bc596dc7fd1e0694ea5795b700ae96
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-26 00:04:06 +0300

    add IFTest

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter9/Stack/IntStack.java
@@ -0,0 +1,6 @@
+package main.java.com.Kuro4KinG.javacore.chapter9.Stack;
+// Определить интерфейс для целочисленного стека
+interface IntStack {
+    void push(int item);    // сохранить элемент в стеке
+    int pop();              // извлечь элемент из стека
+}
