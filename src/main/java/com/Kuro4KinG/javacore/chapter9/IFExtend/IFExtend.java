bad304c02eb2f8d96d771422de6993699b8e7eab
Kurochkin Vladimir <kuro4king@gmail.com>
2020-09-26 14:52:59 +0300

    add IFExtend

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter9/IFExtend/IFExtend.java
@@ -0,0 +1,38 @@
+package main.java.com.Kuro4KinG.javacore.chapter9.IFExtend;
+
+// Один интерфейс может расширять другой
+interface A {
+    void meth1();
+    void meth2();
+}
+
+// Теперь интерфейс B включает в себя методы meth1() и meth2()
+// и добавляет метод meth3()
+interface B extends A {
+    void meth3();
+}
+
+// Этот класс должен реализовать все методы из интерфейсов A и B
+class MyClass implements B {
+    public void meth1() {
+        System.out.println("Реализация метода meth1().");
+    }
+
+    public void meth2() {
+        System.out.println("Реализация метода meth2().");
+    }
+
+    public void meth3() {
+        System.out.println("Реализация метода meth3().");
+    }
+}
+
+class IFExtend {
+    public static void main(String[] args) {
+        MyClass ob = new MyClass();
+
+        ob.meth1();
+        ob.meth2();
+        ob.meth3();
+    }
+}
