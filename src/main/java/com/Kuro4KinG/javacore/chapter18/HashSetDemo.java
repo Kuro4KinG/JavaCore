c8217ab8f041e1bc5498be33496f1497aed66099
Kurochkin Vladimir <kuro4king@gmail.com>
2020-10-12 19:24:09 +0300

    add HashSetDemo

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter18/HashSetDemo.java
@@ -0,0 +1,21 @@
+package com.kuro4king.javacore.chapter18;
+// Продемонстрировать применение класса HashSet
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        // создать хеш-множество
+        HashSet<String> hs = new HashSet<>();
+
+        // ввести элементы в хеш-множество
+        hs.add("Бета");
+        hs.add("Альфа");
+        hs.add("Эта");
+        hs.add("Гамма");
+        hs.add("Эпсилон");
+        hs.add("Омега");
+
+        System.out.println(hs);
+    }
+}
