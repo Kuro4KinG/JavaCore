0d4c355d32ef33e59ed96351e773a89864dff34d
Kurochkin Vladimir <kuro4king@gmail.com>
2020-10-23 19:33:00 +0300

    add HTDemo

---
+++ src/main/java/com/Kuro4KinG/javacore/chapter18/HTDemo.java
@@ -0,0 +1,33 @@
+package com.kuro4king.javacore.chapter18;
+// Продемонстрировать применение класса Hashtable
+
+import java.util.*;
+
+public class HTDemo {
+    public static void main(String[] args) {
+        Hashtable<String, Double> balance = new Hashtable<>();
+        Enumeration<String> names;
+
+        String str;
+        double bal;
+
+        balance.put("Джон Доу", 3434.34);
+        balance.put("Том Смит", 123.22);
+        balance.put("Джейн Бейкер", 1378.00);
+        balance.put("Тод Холл", 99.22);
+        balance.put("Ральф Смит", -19.08);
+
+        // показать все счета в хеш-таблице
+        names = balance.keys();
+        while (names.hasMoreElements()) {
+            str = names.nextElement();
+            System.out.println(str + ": " + balance.get(str));
+        }
+        System.out.println();
+        // внести сумму 1000 на счёт Джона Доу
+        bal = balance.get("Джон Доу");
+        balance.put("Джон Доу", bal + 1000);
+        System.out.println("Новый остаток на счёте Джона Доу: " +
+                balance.get("Джон Доу"));
+    }
+}
