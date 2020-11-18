91b86439e75be1b5e1a59d4501fa1148571c01a0
Kurochkin Vladimir <kuro4king@gmail.com>
2020-11-18 22:57:53 +0300

    modify CopyFile with try-with-resources

--- src/main/java/com/Kuro4KinG/javacore/chapter13/CopyFile.java
+++ src/main/java/com/Kuro4KinG/javacore/chapter13/CopyFile.java
@@ -5,8 +5,6 @@ import java.io.*;
 public class CopyFile {
     public static void main(String[] args) {
         int i;
-        FileInputStream fin = null;
-        FileOutputStream fout = null;
 
         // сначала убедиться, что указаны имена обоих файлов
         if (args.length != 2) {
@@ -15,28 +13,14 @@ public class CopyFile {
         }
 
         // копировать файл
-        try {
-            // попытаться открыть файлы
-            fin = new FileInputStream(args[0]);
-            fout = new FileOutputStream(args[1]);
-
+        try (FileInputStream fin = new FileInputStream(args[0]);
+             FileOutputStream fout = new FileOutputStream(args[1])) {
             do {
                 i = fin.read();
                 if (i != -1) fout.write(i);
             } while (i != -1);
         } catch (IOException e) {
             System.out.println("Ошибка ввода-вывода: " + e);
-        } finally {
-            try {
-                if (fin != null) fin.close();
-            } catch (IOException e2) {
-                System.out.println("Ошибка закрытия файла ввода");
-            }
-            try {
-                if (fout != null) fout.close();
-            } catch (IOException e2) {
-                System.out.println("Ошибка закрытия файла вывода");
-            }
         }
     }
 }
